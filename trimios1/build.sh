0fe4c1162087a89941e586958cb2e2df4ddcce3a
paddu <padmavathi83vattikonda@gmail.com>
2018-02-28 20:45:57 +0530

    modified

--- trimios1/build.sh
+++ trimios1/build.sh
@@ -4,8 +4,7 @@ git add .
 echo "enter commit message"
 read msg
 git commit -m "$msg"
-git remote add origin
-https://github.com/padmavathi83/shell.git
+git remote add origin https://github.com/padmavathi83/shell.git
 git push origin master
 mvn deploy
 mvn sonar:sonar
